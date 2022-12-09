4513f55de279aa5314d482864008504c62e9b486
Marcos <marcoslopes.dev@gmail.com>
2022-12-09 10:23:53 -0300

    metatags

---
+++ components/Metatags/MetaTags.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import React from 'react'
+
+const MetaTags = ({title,description,urlImage}) => {
+  return (
+    <Head>
+        <title>{title}</title>
+        <meta name="title" content={title}/>
+        <meta name="description" content={description}/>
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content={url}/>
+        <meta property="og:title" content={title}/>
+        <meta property="og:description" content={description}/>
+        <meta property="og:image" content={urlImage}/>
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content={description}/>
+        <meta property="twitter:url" content={url}/>
+        <meta property="twitter:title" content={title}/>
+        <meta property="twitter:description" content={description}/>
+        <meta property="twitter:image" content={urlImage}/>
+    </Head>
+  )
+}
+
+export default MetaTags
